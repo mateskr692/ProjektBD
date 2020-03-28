51a1b013d3f857d54dee6b88c170682fc831e011
jkolowca <joanna.kolowca.mf13@gmail.com>
2020-03-28 17:41:06 +0100

    UI base

---
+++ RepairSystem/Presentation.App/Views/Admin/AdminMainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Presentation.App.Views.Admin
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AdminMainWindow.xaml
+    /// </summary>
+    public partial class AdminMainWindow : Window
+    {
+        public AdminMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LeftPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
